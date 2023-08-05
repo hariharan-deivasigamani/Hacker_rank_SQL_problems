6eb605682e1fe0181ead8eab20131d30ed92266c
hariharan.d135@gmail.com <hariharan.d135@gmail.com>
2023-08-05 23:47:00 +0530

    44 45 46

---
+++ sql045.sql
@@ -0,0 +1,17 @@
+'https://www.hackerrank.com/challenges/binary-search-tree-1/problem'
+
+
+select b.n,
+case
+    when b.p is not null then
+        case
+            when
+                (select count(*) from bst where p = b.n) = 0
+                then 'Leaf'
+                else 'Inner'
+            end
+    else
+        'Root'
+end
+from bst b
+order by b.n;
